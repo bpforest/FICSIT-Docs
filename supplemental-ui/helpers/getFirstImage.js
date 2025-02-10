a4a3a818d31cc821e1bb86923f5f3d6d0fa15626
bpforest <144601778+bpforest@users.noreply.github.com>
2025-02-11 01:45:45 +0900

    메타 태그 수정

--- supplemental-ui/helpers/getFirstImage.js
+++ supplemental-ui/helpers/getFirstImage.js
@@ -4,5 +4,8 @@ const IMG_TAG_RX = /<img[^>]+src="([^">]+)"/;
 
 module.exports = (html) => {
     const imgTagMatch = html && html.toString().match(IMG_TAG_RX);
-    return imgTagMatch ? imgTagMatch[1] : null;
+    if (!imgTagMatch) return null;
+
+    const imagePath = imgTagMatch[1].replace(/^(?:\.\.\/)*/, '');
+    return imagePath;
 };
